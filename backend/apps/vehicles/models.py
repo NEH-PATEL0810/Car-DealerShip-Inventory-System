64bdd5e40f4b9c82ba40a11740feeb7bf01e8091
NEH-PATEL0810 <nehp00417@gmail.com>
2026-07-11 17:06:53 +0530

    feat(vehicle): implement Vehicle Model  Implemented vehicle model with required models,validators,etc. and passed all tests.Implemented manually Tests genereated by ChatGPT

--- backend/apps/vehicles/models.py
+++ backend/apps/vehicles/models.py
@@ -1,3 +1,17 @@
+from django.core.validators import MinValueValidator
 from django.db import models
 
 # Create your models here.
+
+class Vehicle(models.Model):
+    make = models.CharField(max_length=100)
+    model = models.CharField(max_length=100)
+    category = models.CharField(max_length=100)
+    price = models.DecimalField(max_digits = 12,decimal_places=2,validators=[MinValueValidator(0)],)
+    quantity = models.PositiveIntegerField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return f"{self.make}{self.model}"
+    
